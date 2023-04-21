2a8cc599914c6d4f97915318a08f54ec6f2db8ea
IXix <philip.considine@btinternet.com>
2023-04-21 17:29:10 +0100

    Fixed CMachineStateVM not noticing child stuff

--- CMachineStateVM.cs
+++ CMachineStateVM.cs
@@ -123,6 +123,7 @@ namespace Snapshot
                 Properties.Add(_state.DataState);
                 DataState = new CMachinePropertyItemVM(_state.DataState, this, _ownerVM, _viewRef);
                 Children.Add(DataState);
+                DataState.PropertyChanged += OnChildPropertyChanged;
             }
 
             if (_state.AttributeStates.ChildProperties.Count > 0)
@@ -130,6 +131,7 @@ namespace Snapshot
                 Properties.UnionWith(_state.AttributeStates.ChildProperties);
                 AttributeStates = new CPropertyStateGroupVM(_state.AttributeStates, this, _ownerVM, _viewRef);
                 Children.Add(AttributeStates);
+                AttributeStates.PropertyChanged += OnChildPropertyChanged;
             }
 
             if (_state.GlobalStates.ChildProperties.Count > 0)
@@ -137,6 +139,7 @@ namespace Snapshot
                 Properties.UnionWith(_state.GlobalStates.ChildProperties);
                 GlobalStates = new CPropertyStateGroupVM(_state.GlobalStates, this, _ownerVM, _viewRef);
                 Children.Add(GlobalStates);
+                GlobalStates.PropertyChanged += OnChildPropertyChanged;
             }
 
             if (_state.TrackStates.ChildProperties.Count > 0)
@@ -147,6 +150,7 @@ namespace Snapshot
                 }
                 TrackStates = new CTrackPropertyStateGroupVM(_state.TrackStates, this, _ownerVM, _viewRef);
                 Children.Add(TrackStates);
+                TrackStates.PropertyChanged += OnChildPropertyChanged;
             }
         }
 
